f7c4841ec48c3a447101ca17ee2e23db78c9890d
Gnarkill33 <gnarkill33@gmail.com>
2025-10-31 20:58:11 +0300

    feat: add add file functionality

--- src/commands/runCommand.js
+++ src/commands/runCommand.js
@@ -1,8 +1,8 @@
 import { dirname, isAbsolute, resolve } from "node:path";
 import { chdir, cwd, stdout } from "node:process";
-import { readdir } from "node:fs/promises";
-import { checkType } from "../utils.js";
+import { readdir, writeFile } from "node:fs/promises";
 import { createReadStream } from "node:fs";
+import { checkType } from "../utils.js";
 
 export const runCommand = async (userInput) => {
  const [command, ...arg] = userInput.trim().split(" ");
@@ -31,7 +31,7 @@ export const runCommand = async (userInput) => {
   case "ls": {
    const currentDir = cwd();
 
-   const dirContent = await readdir(currentDir);
+   const dirContent = await readdir(currentDir, { withFileTypes: true });
 
    const filesToShow = await Promise.all(
     dirContent.map(async (item) => {
@@ -65,6 +65,19 @@ export const runCommand = async (userInput) => {
     console.log("Operation failed");
    });
 
+   break;
+  }
+
+  case "add": {
+   if (arg.length !== 1) console.log("Invalid input");
+
+   const filePath = resolve(cwd(), arg[0]);
+
+   try {
+    await writeFile(filePath, "", { flag: "wx" });
+   } catch {
+    console.log("Operation failed");
+   }
    break;
   }
 
